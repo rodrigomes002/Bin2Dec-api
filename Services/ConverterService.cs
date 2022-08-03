d94da2001765f56c5fe2aa48e85e1354e1ab01a0
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2022-08-02 23:16:36 -0300

    adicionando regex para verificar apenas se 0 e 1 foi digitado

--- Services/ConverterService.cs
+++ Services/ConverterService.cs
@@ -1,5 +1,6 @@
 ﻿using Bin2Dec.DTO;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Bin2Dec.Services
 {
@@ -16,6 +17,9 @@ namespace Bin2Dec.Services
         {
             if (binary.Length > 8) throw new ApplicationException("Só é permitido 8 digitos!");
             if (string.IsNullOrWhiteSpace(binary)) throw new ApplicationException("Preencha o número binário!");
+           
+            var regex = new Regex("[2-9]");
+            if (regex.IsMatch(binary)) throw new ApplicationException("Preencha apenas com 0 e 1");
         }
     }
 }
